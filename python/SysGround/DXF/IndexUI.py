02f6a629d73629fe64943b010d4493f6159de970
hanrker <hanrkery@yeah.net>
2020-09-23 18:03:51 +0800

    2

--- python/SysGround/DXF/IndexUI.py
+++ python/SysGround/DXF/IndexUI.py
@@ -4,20 +4,33 @@ from tkinter import *
 root= Tk()
 root.title('我的第一个Python窗体')
 # root.geometry('1600x900') # 这里的乘号不是 * ，而是小写英文字母 x
-lab = Label(root, text = 'path',  height = 5 , width = 10)
-lab.pack(fill = X)
+
+uiEles = [Label,lab,inp,But,res]
+lab = Label(root, text = 'path',  height = 2 , width = 10)
+# lab.pack(fill = X)
 
 inp = Entry(root, width = 50 )
-inp.pack(side=LEFT)
+# inp.pack()
 
 But = Button(root, text = 'read', width = 50,command= test)
-But.pack()
+# But.pack()
 
 resV = StringVar()
-resV.set("s1")
+resV.set("output")
 res = Label(root,  textvariable = resV,  height = 5 , width = 10)
+putpath = Button(root,  textvariable = resV,  height = 5 , width = 10)
+
 res.pack()
+# putpath.pack()
+
+UIPack(uiEles)
+
+
 root.mainloop()
+
+def UIPack(eles):
+    for ele in eles:
+        ele.pack()
 
 def test():
     print('s')
