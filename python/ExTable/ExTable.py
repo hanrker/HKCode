9f39c2361f013bbb6948ace42bb7be4b4981da8a
hanrker <hanrkery@yeah.net>
2020-09-24 18:31:46 +0800

    excel转换list,转换列+输出excel

--- python/ExTable/ExTable.py
+++ python/ExTable/ExTable.py
@@ -1,26 +1,13 @@
-import xlrd2,win32ui
-import os
-import tkinter
-
+import xlrd2,win32ui,xlwt
+import openpyxl
+import xlutils
+import os.path
+import copy
 
 inputFile = '结果'
 outputPath = ''
 outputFile =''
-workpath =str(os.getcwd()) + "\ExcelToSQL"
-
-# master = tkinter.Tk()
-
-# inputLabel = tkinter.Label(master,text = '输入文件', justify="left")
-# inputButton = tkinter.Button(master,text = '选择文件',command = SelectFile)
-# filename = tkinter.Label(master,text =str(inputFile) )
-
-# inputLabel.pack()
-# inputButton.pack()
-# filename.pack()
-# # 进入消息循环
-# master.mainloop()
-
-inputFile = SelectFile()
+workpath =str(os.getcwd()) + "\ExTable\\"
 
 
 #选择文件
@@ -32,20 +19,125 @@ def SelectFile():
     print(filename)
     return filename
 
+inputFile = SelectFile()
+# print(inputFile)
 
-def WriteExcel(name,path,content):
-    full_path = str(path) + "\\" + str(name)  + '.txt' 
+#读取并输出excel文件为二维 list 
+def ReadExel(path,name,sheet_name):
+    full_path = path + name
     print(full_path)
-    file = open(full_path, 'w')
+    res = []
+    if(os.path.isfile(full_path)):
+        #读取excel文件
+        data = xlrd2.open_workbook(full_path)
+        #读取所有sheet名称
+        sheet = data.sheet_names()
+        
+         # 读取第一个sheet表格
+        table = data.sheet_by_name(sheet_name)
 
-    #如果内容是list，则循环写入
-    if(type(content) ==list ):
-        for i in range(len(content)):
-            file.write(content[i]) #写入内容
-            file.write('\n')
+        if len(sheet)>0:
+            for row in range(table.nrows) :
+                res.append(table.row_values(row))
+                # print(table.row_values(row))
+            print("读取完毕")
+        else:
+             print('Please select file')
     else:
-        file.write(str(content)) #写入内容
-        file.write('\n')
-    # file.write("end") #写入内容
-    print("export file:"+ full_path)
-    file.close()
+        print("导入失败")
+    
+    print(res[0][0])
+    #输出list
+    return res
+
+a = ReadExel(workpath,'test.xlsx','Sheet1')
+WriteExcel_xls_rowx(workpath,'2.xlsx','Sheet1',a)
+
+#转换list
+def Exdata(list,newcol):
+    new_list = copy.deepcopy(list)
+    index = len(list)
+
+    i=0
+    for row in list:
+        print(row)
+        for a in range(len(row)):
+            print(row[newcol[a]])
+            new_list[i][a] = row[newcol[a]]
+            # print(new_list[i][a])
+        # print(new_list[i])
+        i+=1
+    # print(new_list)
+    return new_list
+
+
+#s输出例子
+value3 = [["姓名", "性别", "年龄", "城市", "职业"],
+          ["111", "女", "66", "石家庄", "运维工程师"],
+          ["222", "男", "55", "南京", "饭店老板"],
+          ["333", "女", "27", "苏州", "保安1"],]
+value2= Exdata(value3, [2,3,4,0,1])
+WriteExcel_xls_rowx(workpath,'3.xlsx','Sheet1',value2)
+
+
+def WriteExcel_xls_row(path,name,sheet_name,table): 
+    index = len(table)
+    fullpath = path + name
+
+    workbook = xlwt.Workbook()
+
+    # 创建一个sheet
+    newTable = workbook.add_sheet(sheet_name)
+    # print(table[0][1])
+    # if os.path.isfile(fullpath):
+    #     #复制文件
+    #     workbook = xlrd2.open_workbook(fullpath)
+    #     workbook.save(name)
+    # else:
+    #     #创建新文件
+    #     workbook = xlutils.workbook(encoding = 'utf-8')
+    for r in range(0, index):
+        # print("r"+ str(r))
+        for c in range(0, len(table[r])):
+            # print(str(r)+'-'+ str(c))
+            newTable.write(r , c , table[r][c])
+    if os.path.isdir(path):
+        workbook.save(fullpath)
+        print("xls格式表格写入数据成功！")
+        print('输出路径为：'+ fullpath)
+    else:
+        print("路径不存在:"+ path)
+WriteExcel_xls_row(workpath,'1.xls','sheet',value3)
+
+
+#通过openpyxl 输出xlsx文件
+def WriteExcel_xls_rowx(path,name,sheet_name,table):
+    #获取 行数
+    index = len(table)
+
+    #定义输出路径
+    fullpath = path + name
+
+    # 创建一个文件和sheet
+    workbook = openpyxl.Workbook()
+
+    workbook.active
+    print('s')
+    workbook.remove(workbook['Sheet'])
+    newTable = workbook.create_sheet(sheet_name)
+    print(newTable)
+    # newTable = workbook[sheet_name]
+
+    for r in range(0, index):
+        # print("r"+ str(r))
+        for c in range(0, len(table[r])):
+            print(str(r)+'-'+ str(c))
+            newTable.cell(r+1,c+1,table[r][c])
+    if os.path.isdir(path):
+        workbook.save(fullpath)
+        print("xls格式表格写入数据成功！")
+        print('输出路径为：'+ fullpath)
+    else:
+        print("路径不存在:"+ path)
+#输出2.xlsx 文件 
+WriteExcel_xls_rowx(workpath,'2.xlsx','Sheet1',value3)
--- python/SysGround/DXF/IndexUI.py
+++ python/SysGround/DXF/IndexUI.py
@@ -1,32 +1,39 @@
-import funcs as funcs
 # import ExCAD as cad
 from tkinter import *
+import os
 root= Tk()
 root.title('我的第一个Python窗体')
-# root.geometry('1600x900') # 这里的乘号不是 * ，而是小写英文字母 x
 
-uiEles = [Label,lab,inp,But,res]
-lab = Label(root, text = 'path',  height = 2 , width = 10)
+div = Frame(root,width = 2000, height = 200)
+div.place(x=10,y=20)
+root.geometry('1600x900') # 这里的乘号不是 * ，而是小写英文字母 x
+
+lab = Label(div, text = '输入路径',  height = 2 ,justify = LEFT)
 # lab.pack(fill = X)
+lab.grid(row = 0,column=0 , padx = 2)
 
-inp = Entry(root, width = 50 )
-# inp.pack()
+inp = Entry(div, width = 50 )
+inp.grid(row = 0,column=1)
 
-But = Button(root, text = 'read', width = 50,command= test)
-# But.pack()
+But = Button(div, text = 'read', width = 10,command= test)
+But.grid(row = 0,column=2,padx = 2)
 
 resV = StringVar()
-resV.set("output")
-res = Label(root,  textvariable = resV,  height = 5 , width = 10)
-putpath = Button(root,  textvariable = resV,  height = 5 , width = 10)
+resV.set("打开文件路径")
+res = Label(div,  textvariable = resV,  height = 5 , width = 10 ,justify = LEFT)
 
-res.pack()
-# putpath.pack()
+res.grid(row = 2,column=0)
 
-UIPack(uiEles)
+putpath = Button(div,  textvariable = resV,  height = 1 , width = 50   ,justify = LEFT)
+putpath.grid(row = 2,column=1)
 
-
+#批量打包元素
+# uiEles = [lab,inp,But,res]
+# print(uiEles)
+# UIPack(uiEles) 
 root.mainloop()
+
+input("ss")
 
 def UIPack(eles):
     for ele in eles:
